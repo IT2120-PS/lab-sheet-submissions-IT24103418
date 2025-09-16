a8b100d1a08812fc4b4037b9c3ec91d291668de1
IT24103418 <it24103418@my.sliit.lk>
2025-09-16 18:21:25 +0530

    IT24103418

---
+++ Lab Sheet 07/IT24103418.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\user\\Desktop\\IT24103418")
+
+#1
+punif(10, min=0, max=40, lower.tail=FALSE) - punif(25, min=0, max=40, lower.tail=FALSE)
+
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#3
+1 - pnorm(130, mean=100, sd=15,lower.tail=TRUE)
+
+qnorm(0.95, mean=100, sd=15,lower.tail=TRUE)
+
